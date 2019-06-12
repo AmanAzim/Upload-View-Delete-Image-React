d852ee96ad757a16152ea7f198c07f923f3475a3
Aman <rumman.djuice1@gmail.com>
2019-06-12 21:26:13 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <input type="file"/>
-    </div>
-  );
+class  App extends Component{
+
+  state={
+    selectedFile:null
+  };
+
+  fileSelectHandler=(event)=>{
+    this.setState({selectedFile:event.target.files[0]});
+  };
+  fileUploadHandler=()=>{
+    axios.post('').then(res=>console.log(res)).catch(err=>console.log(err));
+  };
+ render(){
+   return (
+       <div className="App">
+         <div className="container-fluid mt-5">
+           <form>
+             <div className="input-group">
+               <input type="file" className="form-control" onChange={this.fileSelectHandler}/>
+               <button className="btn btn-primary" onClick={this.fileUploadHandler}>Upload Image</button>
+             </div>
+           </form>
+         </div>
+       </div>
+   );
+ }
 }
 
 export default App;

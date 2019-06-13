f74b879ca8dd5ab2a6d65ffef93209667845cb76
Aman <rumman.djuice1@gmail.com>
2019-06-13 20:20:57 +0200

    commit

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
-const functions = require('firebase-functions');
+/*const functions = require('firebase-functions');
 const {Storage} = require('@google-cloud/storage');//have the Google firebase bucket functions
-const projectId='vue-axios-practise-2';
-const gsc = new Storage({projectId});
+
+const gccconfig={
+    projectId:'vue-axios-practise-2',
+    keyFilename:'firebase-adminsdk-iob8d@vue-axios-practise-2.iam.gserviceaccount.com'
+};
+const gsc = new Storage(gccconfig);
 const osTmpdir = require('os-tmpdir');//provides the operating system specific functions that we need to find out the path of the temporal path to store the file for edit;
 const path=require('path');//to constract the temporal path;
 const spawn=require('child-process-promise').spawn; //npm install --save child-process-promise
@@ -57,5 +61,50 @@ exports.metadataUpdated = functions.storage.object().onMetadataUpdate((object, c
     console.log('This is a metadata change event.');
 });
 
+
+
+
+
 //to create a file uploading API end point(url)
-exports.uploadFile=functions.https.onRequest
+const cors=require('cors')({origin:true});
+const Busboy=require('busboy');
+const fs=require('fs');//a node file package
+exports.uploadFile=functions.https.onRequest((req, res)=>{
+
+    cors(req, res, ()=>{
+
+        //For POST request only
+        if(req.method !=='POST'){
+            return res.status(500).json({message:'Not allowed'})
+        }
+
+        const busboy=new Busboy({headers:req.headers});//so that Busboy identifies if incoming request data is form data or not in the server;
+        let uploadData=null;
+        busboy.on('file', (fieldname, file, filename, encoding, mimetype)=>{
+            const filepath=path.join(osTmpdir, filename)//getting the path of the file from the form data that the Busboy extraxted and kept in the temp directory
+
+            uploadData={file:filepath, type:mimetype};
+            file.pipe(fs.createWriteStream(filepath));//this will write the file to firebase storage
+
+        }); //it will be triggered whenever Busboy successfully parses a file from the incoming request
+
+        //When the Busboy finish parsing the data from the incoming request and gets the temporary address of the file. Now it need to save the file in the google cloud storage of Firebase
+        busboy.on('finish', ()=>{
+           const bucket=gsc.createBucket('vue-axios-practise-2.appspot.com')//it this bucket the images will be stored;
+           bucket.upload(uploadData.file, { uploadType:'media', metadata:{ metadata:{ contentType:uploadData.type } } } );
+        }).then(()=>{
+
+                //It handles any type of request. Returns the message if the request is successfull
+                return res.status(200).json({
+                    message:'it worked'
+                });
+        }).catch(err=>{
+            return res.status(500).json({error:err})
+        });
+
+        busboy.end(req.rawBody);
+    })
+
+});*/
+
+
--- src/App.js
+++ src/App.js
@@ -6,25 +6,70 @@ import axios from 'axios';
 class  App extends Component{
 
   state={
-    selectedFile:null
+    filesToUpload:[],
+    images:[]
   };
 
-  fileSelectHandler=(event)=>{
-    this.setState({selectedFile:event.target.files[0]});
+  upload_preview_handler=(event)=>{
+      this.fileSelectHandler_uploader(event);
+      this.fileSelectHandler_viewer(event);
   };
-  fileUploadHandler=()=>{
+
+  fileSelectHandler_uploader=(event)=>{
+      //this.setState({selectedFile:event.target.files[0]});
+      const selectedFile=event.target.files[0];
+      this.setState((prevState)=>{
+          return {
+              fileToUpload:[...prevState.filesToUpload, selectedFile]
+          }
+      });
+      console.log('selectedFile',event.target.files[0]);
+  };
+
+  fileSelectHandler_viewer=(event)=>{
+      event.preventDefault();
+      console.log(event.target.files[0]);
+      const imageFiles = event.target.files; //document.getElementById("image"); //It gives all the uploaded images
+      const filesLength = imageFiles.length; // imageFiles.files.length;
+
+      for(var i = 0; i < filesLength; i++) {
+          let reader = new FileReader();
+          let file = imageFiles[i];
+
+          reader.onloadend = () => {
+              this.setState({images: this.state.images.concat(reader.result)} );
+          }
+
+          reader.readAsDataURL(file);
+      }
+    //this.setState({selectedFile:event.target.files[0]});
+  };
+
+  fileUploadHandler=(event)=>{
+      event.preventDefault();
+
     axios.post('').then(res=>console.log(res)).catch(err=>console.log(err));
   };
+
  render(){
+
    return (
        <div className="App">
          <div className="container-fluid mt-5">
-           <form>
-             <div className="input-group">
-               <input type="file" className="form-control" onChange={this.fileSelectHandler}/>
-               <button className="btn btn-primary" onClick={this.fileUploadHandler}>Upload Image</button>
+             <div className="row p-5">
+                 <form>
+                     <div className="input-group">
+                         <input type="file" className="form-control"  onChange={this.upload_preview_handler}/>
+                         <button className="btn btn-primary" onClick={this.fileUploadHandler}>Upload Image</button>
+                     </div>
+                 </form>
              </div>
-           </form>
+             <div className="row m-5 p-5">
+                 {/*<img src={this.state.images[0]} width="100px" height="100px" />*/}
+                 {this.state.images.map((img, index)=>{
+                     return <img src={img} width="100px" height="100px" style={{margin: '20px'}} key={index}/>
+                 })}
+             </div>
          </div>
        </div>
    );
